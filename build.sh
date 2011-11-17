abcdc0f0e0c5d61609da6d3a88adb60b5eab74aa
Alan Ritter <aritter@aritter-laptop.(none)>
2011-11-16 19:00:07 -0800

    recompiling more code...

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
+cd python/cap
+bash build.sh
 cd hbc/models
 gcc -O3 -lm labels.c stats.c samplib.c LabeledLDA_infer_stdin.c -o LabeledLDA_infer_stdin.out
 cd ../../
 tar xvzf TinySVM-0.09.tar.gz
 cd TinySVM-0.09
 ./configure --prefix=`pwd`/../ && make && make install
-
